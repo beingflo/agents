93b6e9a1572c60e20a1d1f1044437d7e4a5da3bd
Florian Marending <floriama@student.ethz.ch>
2017-03-05 14:46:02 +0100

    Network basic skeleton

---
+++ src/network.rs
@@ -0,0 +1,23 @@
+struct Network {
+
+
+}
+
+impl Network {
+    fn new() -> Network {
+        unimplemented!();
+    }
+
+    fn random() -> Network {
+        unimplemented!();
+    }
+
+    fn add_agent(&mut self, x: i32) { 
+
+    }
+
+    fn add_edge(&mut self, x: i32) {
+
+    }
+
+}
