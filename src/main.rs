d406bb9e7f080a7512befae8c4e9aefdb32c9a10
Florian Marending <floriama@student.ethz.ch>
2017-03-05 22:19:41 +0100

    Changes in Relation API, etc.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,80 @@ use graphics::Renderer;
 
 fn main() {
     let mut renderer = Renderer::new();
-    let mut network = Network::random(50, 0.2);
+    let mut network = Network::new();
+
+    for _ in 0..20 {
+        network.add_agent();
+    }
+
+    for i in 0..20 {
+        let mut min1: f32 = 10.0;
+        let mut min1i = 0;
+        let mut min2: f32 = 10.0;
+        let mut min2i = 0;
+        let mut min3: f32 = 10.0;
+        let mut min3i = 0;
+        let mut min4: f32 = 10.0;
+        let mut min4i = 0;
+
+        let ai = network.get_agent(i).unwrap().pos;
+        for j in 0..20 {
+            let aj = network.get_agent(j).unwrap().pos;
+            let tmp = ((ai.0 - aj.0).powi(2) + (ai.1 - aj.1).powi(2)).sqrt();
+
+            if tmp < min1 {
+                min1 = tmp;
+                min1i = j;
+            }
+        }
+
+        for j in 0..20 {
+            let aj = network.get_agent(j).unwrap().pos;
+            let tmp = ((ai.0 - aj.0).powi(2) + (ai.1 - aj.1).powi(2)).sqrt();
+
+            if j == min1i {
+                continue;
+            }
+
+            if tmp < min2 {
+                min2 = tmp;
+                min2i = j;
+            }
+        }
+
+        for j in 0..20 {
+            let aj = network.get_agent(j).unwrap().pos;
+            let tmp = ((ai.0 - aj.0).powi(2) + (ai.1 - aj.1).powi(2)).sqrt();
+
+            if j == min1i || j == min2i {
+                continue;
+            }
+
+            if tmp < min3 {
+                min3 = tmp;
+                min3i = j;
+            }
+        }
+
+        for j in 0..20 {
+            let aj = network.get_agent(j).unwrap().pos;
+            let tmp = ((ai.0 - aj.0).powi(2) + (ai.1 - aj.1).powi(2)).sqrt();
+
+            if j == min1i || j == min2i || j == min3i {
+                continue;
+            }
+
+            if tmp < min4 {
+                min4 = tmp;
+                min4i = j;
+            }
+        }
+
+        network.add_relation(i, min1i);
+        network.add_relation(i, min2i);
+        network.add_relation(i, min3i);
+        network.add_relation(i, min4i);
+    }
 
     let mut frame = 0;
     loop {
@@ -18,7 +91,7 @@ fn main() {
         println!("{}", frame);
 
         network.draw(&mut renderer);
-        network.smooth(0.001);
+        network.smooth(0.02);
 
         for e in renderer.display.poll_events() {
             use glium::glutin;
--- src/network.rs
+++ src/network.rs
@@ -5,27 +5,26 @@ use graphics::Renderer;
 
 pub struct Network {
     agents: Vec<Agent>,
-    relations: Vec<Relation>,
     rng: rand::ThreadRng,
 }
 
 impl Network {
     pub fn new() -> Network {
-        Network { agents: Vec::new(), relations: Vec::new(), rng: rand::thread_rng() }
+        Network { agents: Vec::new(), rng: rand::thread_rng() }
     }
 
     pub fn random(n: usize, p: f32) -> Network {
         let mut rng = rand::thread_rng();
         let mut network = Network::new();
 
-        for i in 0..n {
+        for _ in 0..n {
             network.add_agent();
         }
 
         for i in 0..n {
-            for j in i..n {
+            for j in 0..n {
                 if get_rand(&mut rng, 0.0, 1.0) < p {
-                    network.add_relation(AgentId(i), AgentId(j));
+                    network.add_relation(i, j);
                 }
             }
         }
@@ -33,63 +32,57 @@ impl Network {
         network
     }
 
-    pub fn add_agent(&mut self) -> AgentId { 
+    pub fn add_agent(&mut self) -> usize { 
         self.agents.push(Agent::new((get_rand(&mut self.rng, -1.0, 1.0),
                                      get_rand(&mut self.rng, -1.0, 1.0)),
                                     0.02,
                                     (0.0, 0.0, 0.0)));
 
-        AgentId(self.agents.len()-1)
+        self.agents.len()-1
     }
 
-    pub fn get_agent(&mut self, a: AgentId) -> &mut Agent {
-        let AgentId(i) = a;
-        &mut self.agents[i]
+    pub fn get_agent(&mut self, a: usize) -> Option<&mut Agent> {
+        if a < self.agents.len() {
+            return Some(&mut self.agents[a])
+        }
+        None
     }
 
-    pub fn add_relation(&mut self, src: AgentId, dest: AgentId) -> RelationId {
-        self.relations.push(Relation { source: src, target: dest, color: (0.0, 0.0, 0.0) });
-
-        RelationId(self.relations.len()-1)
+    pub fn add_relation(&mut self, src: usize, dest: usize) {
+        self.agents[src].relations.push(Relation { target: dest, color: (0.0, 0.0, 0.0) });
     }
 
-    pub fn get_relation(&mut self, r: RelationId) -> &mut Relation {
-        let RelationId(i) = r;
-        &mut self.relations[i]
+    pub fn get_relation(&mut self, a: usize, b: usize) -> Option<&mut Relation> {
+        let mut cnt = None;
+        for (i, r) in self.agents[a].relations.iter().enumerate() {
+            if r.target == b {
+                cnt = Some(i);
+            }
+        }
+        if cnt.is_some() {
+            Some(&mut self.agents[a].relations[cnt.unwrap()])
+        } else {
+            None
+        }
     }
 
     pub fn smooth(&mut self, dt: f32) {
-        let k = 0.01;
-        let rest = 1.0;
+        let rest = 0.5;
 
         for i in 0..self.agents.len() {
             let posi = self.agents[i].pos;
 
             let mut aggregate_force = (0.0, 0.0);
 
-            for j in self.relations.iter() {
-                if j.source == AgentId(i) {
-                    let posj = self.agents[j.target.0].pos;
+            for j in self.agents[i].relations.iter() {
+                let posj = self.agents[j.target].pos;
 
-                    let dist = ((posj.0 - posi.0).powi(2) + (posj.1 - posi.1).powi(2)).sqrt();
-                    let dir = (posj.0 - posi.0, posj.1 - posi.1);
+                let dist = ((posj.0 - posi.0).powi(2) + (posj.1 - posi.1).powi(2)).sqrt();
+                let dir = (posj.0 - posi.0, posj.1 - posi.1);
 
-                    aggregate_force.0 += (dist - rest) * dir.0;
-                    aggregate_force.1 += (dist - rest) * dir.1;
-                }
-                if j.target == AgentId(i) {
-                    let posj = self.agents[j.source.0].pos;
-
-                    let dist = ((posj.0 - posi.0).powi(2) + (posj.1 - posi.1).powi(2)).sqrt();
-                    let dir = (posj.0 - posi.0, posj.1 - posi.1);
-
-                    aggregate_force.0 += (dist - rest) * dir.0;
-                    aggregate_force.1 += (dist - rest) * dir.1;
-                }
-
+                aggregate_force.0 += (dist - rest) * dir.0;
+                aggregate_force.1 += (dist - rest) * dir.1;
             }
-
-            println!("{:?}", aggregate_force);
 
             aggregate_force.0 *= dt;
             aggregate_force.1 *= dt;
@@ -103,7 +96,11 @@ impl Network {
         renderer.clear_color(1.0, 1.0, 1.0);
 
         self.agents.iter().map(|ref a| renderer.draw_circle(a.pos, a.r, a.color)).count();
-        self.relations.iter().map(|ref r| renderer.draw_line(self.agents[r.source.0].pos, self.agents[r.target.0].pos, r.color)).count();
+        for i in self.agents.iter() {
+            for j in i.relations.iter() {
+                renderer.draw_line(i.pos, self.agents[j.target].pos, j.color);
+            }
+        }
 
         renderer.end_frame();
     }
@@ -115,14 +112,16 @@ fn get_rand(rng: &mut rand::ThreadRng, a: f32, b: f32) -> f32 {
 }
 
 pub struct Agent {
-    pos: (f32, f32),
+    pub pos: (f32, f32),
     r: f32,
     color: (f32, f32, f32),
+
+    relations: Vec<Relation>,
 }
 
 impl Agent {
     fn new(pos: (f32, f32), r: f32, color: (f32, f32, f32)) -> Agent {
-        Agent { pos: pos, r: r, color: color }
+        Agent { pos: pos, r: r, color: color, relations: Vec::new() }
     }
 
     fn set_color(&mut self, col: (f32, f32, f32)) {
@@ -138,22 +137,13 @@ impl Agent {
     }
 }
 
-#[derive(Copy, Clone, PartialEq, Eq)]
-pub struct AgentId(usize);
-
 pub struct Relation {
-    source: AgentId,
-    target: AgentId,
+    target: usize,
     color: (f32, f32, f32),
 }
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-pub struct RelationId(usize);
 
 impl Relation {
     fn set_color(&mut self, col: (f32, f32, f32)) {
         self.color = col;
     }
 }
-
-
