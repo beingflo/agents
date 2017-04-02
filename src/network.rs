703f46168233feb1116fa6984d8897545bae84c4
Florian Marending <floriama@student.ethz.ch>
2017-04-02 17:49:09 +0200

    Add centering force

--- src/network.rs
+++ src/network.rs
@@ -81,13 +81,13 @@ impl Network {
     // force to keep non-adjacent vertices from clustering
     pub fn smooth(&mut self, dt: f32) {
         // Rest length of springs
-        let rest = 2.0;
+        let rest = 1.0;
 
         // Damping factor
         let d = 0.2;
 
         // Stiffness of spring
-        let k = 2.0;
+        let k = 1.0;
 
         // Coulomb's constant
         let k_e = 2.0;
@@ -95,9 +95,13 @@ impl Network {
         // Distance lower bound for Coulomb
         let low = 0.1;
 
+        // Centering coefficient
+        let cent = 0.2;
+
         for i in 0..self.agents.len() {
             let posi = self.agents[i].pos;
 
+            // Spring force
             let mut f_spring = (0.0, 0.0);
             for j in self.agents[i].relations.iter() {
                 let posj = self.agents[j.target].pos;
@@ -108,10 +112,8 @@ impl Network {
                 f_spring.0 += k*(dist - rest) * (dir.0 / dist);
                 f_spring.1 += k*(dist - rest) * (dir.1 / dist);
             }
-            // Damping
-            f_spring.0 -= d*self.agents[i].vel.0;
-            f_spring.1 -= d*self.agents[i].vel.1;
 
+            // Coulomb force
             let mut f_coulomb = (0.0, 0.0);
             for j in 0..self.agents.len() {
                 if i == j {
@@ -132,7 +134,16 @@ impl Network {
                 f_coulomb.1 += -k_e/(dist_bound*dist_bound) * (dir.1 / dist_bound);
             }
 
-            let f = (f_spring.0 + f_coulomb.0, f_spring.1 + f_coulomb.1);
+            let mut f = (f_spring.0 + f_coulomb.0, f_spring.1 + f_coulomb.1);
+
+            // Damping
+            f.0 -= d*self.agents[i].vel.0;
+            f.1 -= d*self.agents[i].vel.1;
+
+            // Centering force
+            // -> to keep the vertices from floating away
+            f.0 += -cent*posi.0;
+            f.1 += -cent*posi.1;
 
             self.agents[i].vel.0 += dt*f.0;
             self.agents[i].vel.1 += dt*f.1;
