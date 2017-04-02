b1743052f616089cfbf95097428db43831c3f024
Florian Marending <floriama@student.ethz.ch>
2017-04-02 19:39:33 +0200

    Separate phyisics component out

--- src/network.rs
+++ src/network.rs
@@ -26,7 +26,7 @@ impl Network {
             for j in 0..side {
                 let p1 = get_rand(&mut rng, -0.2, 0.2);
                 let p2 = get_rand(&mut rng, -0.2, 0.2);
-                network.agents.push(Agent::new((i as f32 + p1, j as f32 + p2), 0.05, (0.0, 0.0, 0.0)));
+                network.agents.push(Agent::new((i as f32 + p1, j as f32 + p2), 0.1, (0.0, 0.0, 0.0)));
             }
         }
 
@@ -65,7 +65,7 @@ impl Network {
         self.agents.push(Agent::new((   get_rand(&mut self.rng, -10.0, 10.0),
                                         get_rand(&mut self.rng, -10.0, 10.0)
                                     ),
-                                    0.05,
+                                    0.1,
                                     (0.0, 0.0, 0.0)
                                     )
                          );
@@ -91,8 +91,8 @@ impl Network {
     fn total_vel(&self) -> f32 {
         let mut total_vel = 0.0;
         for i in self.agents.iter() {
-            total_vel += i.vel.0.abs();
-            total_vel += i.vel.1.abs();
+            total_vel += i.physics.vel.0.abs();
+            total_vel += i.physics.vel.1.abs();
         }
         total_vel
     }
@@ -120,12 +120,12 @@ impl Network {
         let cent = 0.2;
 
         for i in 0..self.agents.len() {
-            let posi = self.agents[i].pos;
+            let posi = self.agents[i].physics.pos;
 
             // Spring force
             let mut f_spring = (0.0, 0.0);
             for j in self.agents[i].relations.iter() {
-                let posj = self.agents[j.target].pos;
+                let posj = self.agents[j.target].physics.pos;
 
                 let dist = ((posj.0 - posi.0).powi(2) + (posj.1 - posi.1).powi(2)).sqrt();
                 let dir = (posj.0 - posi.0, posj.1 - posi.1);
@@ -140,7 +140,7 @@ impl Network {
                 if i == j {
                     continue;
                 }
-                let posj = self.agents[j].pos;
+                let posj = self.agents[j].physics.pos;
 
                 let dist = ((posj.0 - posi.0).powi(2) + (posj.1 - posi.1).powi(2)).sqrt();
                 let dir = (posj.0 - posi.0, posj.1 - posi.1);
@@ -158,19 +158,19 @@ impl Network {
             let mut f = (f_spring.0 + f_coulomb.0, f_spring.1 + f_coulomb.1);
 
             // Damping
-            f.0 -= d*self.agents[i].vel.0;
-            f.1 -= d*self.agents[i].vel.1;
+            f.0 -= d*self.agents[i].physics.vel.0;
+            f.1 -= d*self.agents[i].physics.vel.1;
 
             // Centering force
             // -> to keep the vertices from floating away
             f.0 += -cent*posi.0;
             f.1 += -cent*posi.1;
 
-            self.agents[i].vel.0 += dt*f.0;
-            self.agents[i].vel.1 += dt*f.1;
+            self.agents[i].physics.vel.0 += dt*f.0;
+            self.agents[i].physics.vel.1 += dt*f.1;
 
-            self.agents[i].pos.0 += dt*self.agents[i].vel.0;
-            self.agents[i].pos.1 += dt*self.agents[i].vel.1;
+            self.agents[i].physics.pos.0 += dt*self.agents[i].physics.vel.0;
+            self.agents[i].physics.pos.1 += dt*self.agents[i].physics.vel.1;
         }
     }
 
@@ -178,10 +178,10 @@ impl Network {
         renderer.begin_frame();
         renderer.clear_color(1.0, 1.0, 1.0);
 
-        self.agents.iter().map(|ref a| renderer.draw_circle(a.pos, a.r, a.color)).count();
+        self.agents.iter().map(|ref a| renderer.draw_circle(a.physics.pos, a.physics.r, a.physics.color)).count();
         for i in self.agents.iter() {
             for j in i.relations.iter() {
-                renderer.draw_line(i.pos, self.agents[j.target].pos, j.color);
+                renderer.draw_line(i.physics.pos, self.agents[j.target].physics.pos, j.color);
             }
         }
 
@@ -194,22 +194,51 @@ fn get_rand(rng: &mut rand::ThreadRng, a: f32, b: f32) -> f32 {
 }
 
 pub struct Agent {
-    pos: (f32, f32),
-    vel: (f32, f32),
-
-    r: f32,
-    color: (f32, f32, f32),
+    physics: PhysicsComponent,
+    property: AbstractComponent,
 
     relations: Vec<Relation>,
 }
 
-impl Agent {
-    fn new(pos: (f32, f32), r: f32, color: (f32, f32, f32)) -> Agent {
-        Agent {
+struct PhysicsComponent {
+    pos: (f32, f32),
+    vel: (f32, f32),
+    r: f32,
+    color: (f32, f32, f32),
+}
+
+impl PhysicsComponent {
+    fn new(pos: (f32, f32), vel: (f32, f32), r: f32, color: (f32, f32, f32)) -> PhysicsComponent {
+        PhysicsComponent {
             pos: pos,
-            vel: (0.0, 0.0),
+            vel: vel,
             r: r,
             color: color,
+        }
+    }
+
+}
+
+struct AbstractComponent {
+    ph: i32,
+}
+
+impl AbstractComponent {
+    fn new() -> AbstractComponent {
+        AbstractComponent { ph: 0 }
+    }
+}
+
+
+impl Agent {
+    fn new(pos: (f32, f32), r: f32, color: (f32, f32, f32)) -> Agent {
+        let pc = PhysicsComponent::new(pos, (0.0, 0.0), r, color);
+        let ac = AbstractComponent::new();
+
+        Agent {
+            physics: pc,
+            property: ac,
+            
             relations: Vec::new(),
         }
     }
